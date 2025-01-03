54f92a5020eb343e2b69f536c5e1ff3565255f2c
Aleksandr528012 <ask528012@gmail.com>
2025-01-04 01:48:53 +0300

    initial commit

---
+++ Source.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+struct bankaccount
+{
+
+}; 
+
+int main(int argc, char** atgv) {
+    setlocale(LC_ALL, "Russian");
+    int m;
+
+    /*do {
+
+        std::cout << "введите номер месяца: ";
+        std::cin >> m;
+        Months m1 = static_cast<Months>(m);
+
+        switch (m1) {
+        case Months::Jan:
+            std::cout << "Январь" << std::endl;
+            break;
+        case Months::Feb:
+            std::cout << "Февраль" << std::endl;
+            break;
+        case Months::Marth:
+            std::cout << "Март" << std::endl;
+            break;
+        case Months::April:
+            std::cout << "Апрель" << std::endl;
+            break;
+        case Months::May:
+            std::cout << "Май" << std::endl;
+            break;
+        case Months::June:
+            std::cout << "Июнь" << std::endl;
+            break;
+        case Months::Jule:
+            std::cout << "Июль" << std::endl;
+            break;
+        case Months::Aug:
+            std::cout << "Август" << std::endl;
+            break;
+        case Months::Sep:
+            std::cout << "Сентябрь" << std::endl;
+            break;
+        case Months::Oct:
+            std::cout << "Октябрь" << std::endl;
+            break;
+        case Months::Nov:
+            std::cout << "Ноябрь" << std::endl;
+            break;
+        case Months::Dec:
+            std::cout << "Декабрь" << std::endl;
+            break;
+        }
+    } while (m > 0);*/
+
+}
